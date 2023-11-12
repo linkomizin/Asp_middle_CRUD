9c06301a16d6764da7fccd005d130936ba1126b4
linkomizin <linkomizin@gmail.com>
2023-11-12 19:44:57 +0300

    Add core? services, contracts, abstraction

---
+++ Domain/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class OrderItem
+
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public decimal Quautity { get; set; }
+        public string Unit { get; set; }
+    }
+}
