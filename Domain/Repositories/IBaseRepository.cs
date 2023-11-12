ab2567f7e71ca5ffa7884f46d44f201ac9d11852
linkomizin <linkomizin@gmail.com>
2023-11-12 22:46:59 +0300

    add base Irepository<T>

---
+++ Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        void Insert(T t);
+        void Remove(T t);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
