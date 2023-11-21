7d8c6ff1f3bc1459438c42b3c0405c1c7cf6ae4c
linkomizin <linkomizin@gmail.com>
2023-11-21 17:23:29 +0300

    implement ProviderRepository

---
+++ Persisstence/Repositories/ProviderRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class ProviderRepository : IProviderRepository
+    {
+        private RepositoryDbContext _dbContext;
+        public ProviderRepository(RepositoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<IEnumerable<Provider>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.Run(() => _dbContext.Providers.AsEnumerable());
+        }
+
+
+        public Task<Provider> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return Task.Run(() => _dbContext.Providers.FirstOrDefault(p => p.Id == id));
+        }
+
+        public void Insert(Provider t)
+        {
+            _dbContext.Providers.Add(t);
+        }
+
+        public void Remove(Provider t)
+        {
+            _dbContext.Providers.Remove(t);
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.Run(() => _dbContext.SaveChanges());
+        }
+    }
+}
