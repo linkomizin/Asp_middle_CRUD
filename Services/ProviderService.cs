cbb60b03c1d9fc837252d58f8c0c3086ab39665d
linkomizin <linkomizin@gmail.com>
2023-11-21 17:09:48 +0300

    implement ProviderService

--- Services/ProviderService.cs
+++ Services/ProviderService.cs
@@ -2,6 +2,7 @@
 using Domain.Repositories;
 using Services.Abstractions;
 using Mapster;
+using Domain.Entities;
 
 namespace Services;
 
@@ -27,21 +28,45 @@ public class ProviderService : IProviderServices
 
     public async Task<ProviderDto> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var finded = await _repositoryManager.ProviderRepository.GetByIdAsync(id, cancellationToken);
+        if (finded is null)
+        {
+            // edit this to custom Exception!!
+            throw new Exception();
+        }
+        var providerDto = finded.Adapt<ProviderDto>();
+        return providerDto;
     }
 
     public async Task<ProviderDto> CreateAsync(ProviderForCreationDto providerForCreationDto, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var provider = providerForCreationDto.Adapt<Provider>();
+        _repositoryManager.ProviderRepository.Insert(provider);
+        await _repositoryManager.ProviderRepository.SaveChangesAsync(cancellationToken);
+        return provider.Adapt<ProviderDto>();
     }
 
     public async Task UpdateAsync(int id, ProviderDto providerDto, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var provider = await _repositoryManager.ProviderRepository.GetByIdAsync(id, cancellationToken);
+        if (provider is null)
+        {
+            // edit this to custom Exception!!
+            throw new Exception();
+        }
+        provider.Name = providerDto.Name;
+        await _repositoryManager.ProviderRepository.SaveChangesAsync(cancellationToken);
     }
 
     public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var provider = await _repositoryManager.ProviderRepository.GetByIdAsync(id, cancellationToken);
+        if(provider is null)
+        {
+            // edit this to custom Exception!!
+            throw new Exception();
+        }
+        _repositoryManager.ProviderRepository.Remove(provider);
+        await _repositoryManager.ProviderRepository.SaveChangesAsync();
     }
 }
